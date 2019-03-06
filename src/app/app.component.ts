81516d4b8da66c551d58676f6c3b58777f2ef17b
Adam Duren <adamduren@gmail.com>
2019-03-06 08:08:45 -0800

    This commit works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
 
+import {
+  animate,
+  keyframes,
+  query,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+
+export const appAnimations = [
+  trigger('boxTransition', [
+    state(
+      'red',
+      style({
+        backgroundColor: 'red'
+      }),
+    ),
+
+    state(
+      'blue',
+      style({
+        backgroundColor: 'blue'
+      }),
+    ),
+
+    transition('blue <=> red', [animate('250ms ease-out')]),
+  ]),
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [appAnimations],
 })
 export class AppComponent {
-  title = 'angular-animation-issue';
+  public boxColor = 'red';
+
+  public toggleMapMode() {
+    this.boxColor = this.boxColor === 'red' ? 'blue' : 'red';
+  }
 }
